2822de16043b6465b6e23c8b9bebdbfe9681a74e
Mitchell Cooper <cooper879@msn.com>
2013-03-19 19:07:02 -0400

    f

--- DuckingNinja/ServerManager.pm
+++ DuckingNinja/ServerManager.pm
@@ -87,9 +87,9 @@ sub http_2_servers {
     }
     
     my $success = DuckingNinja::db_do(
-        'UPDATE {servers} SET index = ? WHERE name = ?',
-        'last',
+        'UPDATE {servers} SET index = ? WHERE index = ?',
         $index_used
+        'last'
     ) or die $DBI::errstr."\n";
     
     # failed to set.
