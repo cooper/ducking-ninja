1caeb19a2978c51be39196cfaa1b1734c7538a87
Mitchell Cooper <cooper879@msn.com>
2013-04-20 02:01:52 -0400

    must be set

--- DuckingNinja/HTTPd/nginx.pm
+++ DuckingNinja/HTTPd/nginx.pm
@@ -99,7 +99,7 @@ sub handle_request {
     %postVariables = %$postVariables if $postVariables && ref $postVariables eq 'HASH';
     
     # currently only API version 2.0 is supported.
-    return &HTTP_INTERNAL_SERVER_ERROR if $api_version != 2.0;
+    return &HTTP_INTERNAL_SERVER_ERROR if $api_version && $api_version != 2.0;
     $api_prefix = 2;
     
     # server manager does not handle this...
